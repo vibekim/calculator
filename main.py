e5b0c0af9828453a7fb151ebd97aeb25a6085451
vibekim <vibekode2025@gmail.com>
2026-02-15 21:26:48 +0900

    Add icon.png file and push button with hadler function in main.py

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
-# ch 4.2.1 main.py
+# ch 4.2.3 main.py
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,  QMessageBox)
+from PyQt5.QtGui import QIcon
+
+
 class Calculator(QWidget):
-
-  def __init__(self):
-    super().__init__()
-    self.initUI()
-
-  def initUI(self):
-    self.setWindowTitle('Calculator')
-    self.resize(256,256)
-    self.show()
-
-if __name__=='__main__':
-  app = QApplication(sys.argv)
-  view = Calculator()
-  sys.exit(app.exec_())
+    
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        
+    def initUI(self):
+        self.btn1=QPushButton('Message',self)
+        self.btn1.clicked.connect(self.activateMessage)
+        
+        vbox=QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addWidget(self.btn1)
+        vbox.addStretch(1)
+        
+        self.setLayout(vbox)
+        
+        self.setWindowTitle('Calculator')
+        self.setWindowIcon(QIcon('icon.png'))
+        self.resize(256,256)
+        self.show()
+        
+    def activateMessage(self):
+        QMessageBox.information(self,"information","Button clicked!")
+        
+        
+if __name__ == '__main__':
+    app=QApplication(sys.argv)
+    view=Calculator()
+    sys.exit(app.exec_())
